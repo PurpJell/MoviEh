0e36ca8cf037824139b13a8174c4c77409d3f94d
PurpJell <115624117+PurpJell@users.noreply.github.com>
2024-10-21 22:50:27 +0300

    Added duration to films

--- backend/restapi/views/questionnaire.py
+++ backend/restapi/views/questionnaire.py
@@ -66,7 +66,8 @@ class QuestionnaireAPIView(APIView):
                 "genre": "Crime, Drama",
                 "year": 1972,
                 "rating": 9.2,
-                "description": "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son."  # noqa: E501
+                "duration": "175 min",
+                "description": "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",  # noqa: E501
             },
             {
                 "id": 1,
@@ -74,7 +75,8 @@ class QuestionnaireAPIView(APIView):
                 "genre": "Drama",
                 "year": 1994,
                 "rating": 9.3,
-                "description": "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency."  # noqa: E501
+                "duration": "142 min",
+                "description": "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",  # noqa: E501
             },
             {
                 "id": 2,
@@ -82,7 +84,8 @@ class QuestionnaireAPIView(APIView):
                 "genre": "Action, Adventure, Sci-Fi",
                 "year": 2010,
                 "rating": 8.8,
-                "description": "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a CEO."  # noqa: E501
+                "duration": "148 min",
+                "description": "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a CEO.",  # noqa: E501
             },
             {
                 "id": 3,
@@ -90,7 +93,8 @@ class QuestionnaireAPIView(APIView):
                 "genre": "Action, Crime, Drama",
                 "year": 2008,
                 "rating": 9.0,
-                "description": "When the menace known as the Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham. The Dark Knight must accept one of the greatest psychological and physical tests of his ability to fight injustice."  # noqa: E501
+                "duration": "152 min",
+                "description": "When the menace known as the Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham. The Dark Knight must accept one of the greatest psychological and physical tests of his ability to fight injustice.",  # noqa: E501
             },
             {
                 "id": 4,
@@ -98,7 +102,8 @@ class QuestionnaireAPIView(APIView):
                 "genre": "Crime, Drama",
                 "year": 1994,
                 "rating": 8.9,
-                "description": "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption."  # noqa: E501
+                "duration": "154 min",
+                "description": "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",  # noqa: E501
             }
         ]
 
