11d270e9fcdf5747ef6c11afa6031f3c43679bbc
Rimvydas Čivilis <71722408+rimvydascivilis@users.noreply.github.com>
2024-11-05 21:42:59 +0200

    Fix get

--- backend/restapi/views/questionnaire.py
+++ backend/restapi/views/questionnaire.py
@@ -1,18 +1,15 @@
 from rest_framework.views import APIView
-from rest_framework import status
 from rest_framework.permissions import AllowAny
-from ..data_models import Questionnaire
+import json
+from django.conf import settings
 from django.http import JsonResponse
 
 
 class QuestionnaireAPIView(APIView):
     permission_classes = [AllowAny]
 
-    def __init__(self):
-        self.questionnaire = Questionnaire()
-
     def get(self, request):
-        return JsonResponse(
-            {"questions": self.questionnaire.questions},
-            status=status.HTTP_200_OK
-        )
+        path = settings.BASE_DIR / 'restapi' / 'data' / 'questions.json'
+        with open(path, 'r') as file:
+            questions = json.load(file)
+        return JsonResponse(questions)
