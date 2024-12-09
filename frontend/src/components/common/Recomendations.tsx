9277bf7cc05772e5b82f69c224a3a33672f477a3
Rimvydas Čivilis <71722408+rimvydascivilis@users.noreply.github.com>
2024-12-09 21:37:07 +0200

    Add Film component and refactor Recommendations to use it

--- frontend/src/components/common/Recomendations.tsx
+++ frontend/src/components/common/Recomendations.tsx
@@ -111,24 +111,7 @@ const Recommendations: React.FC<IRecommendationsProps> = ({
         {filteredRecommendations.length > 0 ? (
           filteredRecommendations.map((film, index) => (
             <Col key={index} span={8}>
-              <Card
-                title={film.title}
-                bordered={false}
-                hoverable
-                style={{marginBottom: '16px'}}>
-                <Text strong>Year:</Text> <Text>{film.year}</Text>
-                <br />
-                <Text strong>Genres:</Text>{' '}
-                <Text>{film.genres.join(', ')}</Text>
-                <br />
-                <Text strong>Rating:</Text> <Text>{film.rating}</Text>
-                <br />
-                <Text strong>Duration:</Text> <Text>{film.duration} min.</Text>
-                <br />
-                <Text strong>Description:</Text>{' '}
-                <Text>{film.shortDescription}</Text>
-                <br />
-              </Card>
+              <Film film={film} />
             </Col>
           ))
         ) : (
